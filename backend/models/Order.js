ab9258f2299630cfd68f7b4922db527aaf70c538
Santosh Kumar Singh <230101120237@centurionuniv.edu.in>
2026-09-03 19:49:00 +0530

    Delete backend/models directory

--- backend/models/Order.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const OrderSchema = new mongoose.Schema({
-  patientId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  pharmacyId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  items: [{ name:String, quantity:Number }],
-  status: { type: String, enum: ['pending','processed','dispatched','delivered'], default: 'pending'},
-  createdAt: { type: Date, default: Date.now }
-});
-module.exports = mongoose.model('Order', OrderSchema);
